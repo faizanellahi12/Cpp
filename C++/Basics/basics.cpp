924e508fbd4d9a5d32b24b0872af2b1e388071eb
Faizan <faizane619@gmail.com>
2025-03-16 23:18:21 +0530

    Add files via upload

---
+++ C++/Basics/basics.cpp
@@ -0,0 +1,91 @@
+PRINT HELLO WORLD
+
+
+// #include<iostream>
+// using name space std;
+// int main(){
+//     cout<<"hello world ";
+//     return 0;
+// }
+
+
+
+
+PRINT SERIES FROM  1 TO 20
+
+// include<iostrem>
+// using name space std;
+// int main(){
+//     int i=0;
+//     for(i=0;i<20;i++){
+//         cout<<i;
+    
+//     }
+//     return 0;
+// }
+
+
+ print ony even number from one to 20
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int i =1;
+//     for (i=1;i<20;i++){
+//         if(i%2==0){
+//             cout<<i;
+
+//         }
+// }
+// return 0;
+//     }
+
+
+
+
+sum of array with 9 elements 
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[9]={1,2,2,3,4,5,6,8,9};
+//     int sum=0;
+//     for(int i=0;i<9;i++){
+//         sum=sum+arr[i];
+//     }
+//     cout<<sum;
+//     return 0;
+
+// }
+
+
+ print all odd numbers in array with 9 elements 
+
+// #include<iostream>
+// using name space std;
+//     int main(){
+
+//         int arr[9]={1,2,3,4,5,6,7,8,9,};
+//         for(int i=0;i<9;i++){
+//             if(i%2==0){
+//                 cout<<arr[i];
+//             }
+//         }
+//     }
+//     return 0;
+// }
+  PRINT ALL ODD NUMBERS IN AN ARRAY HAVING 10 ELEMENTS 
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[10]={12,3,4,5,6,7,8,9,10};
+//     for(int i=0;i<10;i++){
+//         if(i%2!=0){
+//             cout<<i;
+//         }
+//     }
+//     return 0;
+// }
+
