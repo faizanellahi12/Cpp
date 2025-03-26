12f8c39c907998dfb2e88af3aa0ed4cf4bf465cb
Faizan elahi <faizane619@gmail.com>
2025-03-26 20:13:35 +0530

    .

--- C++/Basics/basics.cpp
+++ C++/Basics/basics.cpp
@@ -25,7 +25,7 @@ PRINT SERIES FROM  1 TO 20
 // }
 
 
- print ony even number from one to 20
+ print any even number from one to 20
 
 // #include <iostream>
 // using namespace std;
@@ -38,54 +38,3 @@ PRINT SERIES FROM  1 TO 20
 
 //         }
 // }
-// return 0;
-//     }
-
-
-
-
-sum of array with 9 elements 
-
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int arr[9]={1,2,2,3,4,5,6,8,9};
-//     int sum=0;
-//     for(int i=0;i<9;i++){
-//         sum=sum+arr[i];
-//     }
-//     cout<<sum;
-//     return 0;
-
-// }
-
-
- print all odd numbers in array with 9 elements 
-
-// #include<iostream>
-// using name space std;
-//     int main(){
-
-//         int arr[9]={1,2,3,4,5,6,7,8,9,};
-//         for(int i=0;i<9;i++){
-//             if(i%2==0){
-//                 cout<<arr[i];
-//             }
-//         }
-//     }
-//     return 0;
-// }
-  PRINT ALL ODD NUMBERS IN AN ARRAY HAVING 10 ELEMENTS 
-
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int arr[10]={12,3,4,5,6,7,8,9,10};
-//     for(int i=0;i<10;i++){
-//         if(i%2!=0){
-//             cout<<i;
-//         }
-//     }
-//     return 0;
-// }
-
---
+++ C++/Loops/loops.cpp
@@ -0,0 +1,17 @@
+WHILE LOOP
+
+#include<iostream>
+using namespace std;
+int main(){
+    int teacups; 
+
+
+    cout<<"enter the number of tea cups to serve";
+    cin>> teacups;
+    while(teacups > 0){
+cout<<"servining cup of tea /n"<<teacups<<"reamaning"<<endl;
+teacups--;
+    }
+    cout<<"all tea cups are served"<<endl;
+    return 0;
+}}
