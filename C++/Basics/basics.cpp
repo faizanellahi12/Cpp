df662934cdeaacd915b7e356104d2cff57063c89
Faizan elahi <faizane619@gmail.com>
2025-03-27 21:39:41 +0530

    Add files via upload

--- C++/Basics/basics.cpp
+++ C++/Basics/basics.cpp
@@ -1,55 +1,85 @@
-PRINT HELLO WORLD
+// PRINT HELLO WORLD
 
 
-// #include<iostream>
-// using name space std;
-// int main(){
-//     cout<<"hello world ";
-//     return 0;
-// }
+// // #include<iostream>
+// // using name space std;
+// // int main(){
+// //     cout<<"hello world ";
+// //     return 0;
+// // }
 
 
 
 
-PRINT SERIES FROM  1 TO 20
+// PRINT SERIES FROM  1 TO 20
 
-// include<iostrem>
-// using name space std;
-// int main(){
-//     int i=0;
-//     for(i=0;i<20;i++){
-//         cout<<i;
+// // include<iostrem>
+// // using name space std;
+// // int main(){
+// //     int i=0;
+// //     for(i=0;i<20;i++){
+// //         cout<<i;
     
-//     }
-//     return 0;
-// }
+// //     }
+// //     return 0;
+// // }
 
 
- print any even number from one to 20
+// //  print any even number from one to 20
+
+// // #include <iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int i =1;
+// //     for (i=1;i<20;i++){
+// //         if(i%2==0){
+// //             cout<<i;
+
+// //         }
+// // }
+
+
+
 
 // #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int i =1;
-//     for (i=1;i<20;i++){
-//         if(i%2==0){
-//             cout<<i;
+// // using namespace std;
+// // int main()
+// // {
+// //     int i =1;
+// //     for (i=1;i<20;i++){
+// //         if(i%2==0){
+// //             cout<<i;
 
-//         }
-// }
+// //         }
+// // }
+
 
 
 
 
 #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int i =1;
-//     for (i=1;i<20;i++){
-//         if(i%2==0){
-//             cout<<i;
+using namespace std;
 
-//         }
-// }
+// Function to add two numbers in centimeters
+float addCentimeters(float cm1, float cm2) {
+    return cm1 + cm2;
+}
+
+int main() {
+    float num1, num2;
+
+    // Input: Get two numbers from the user
+    cout << "Enter the first number in centimeters: ";
+    cin >> num1;
+    cout << "Enter the second number in centimeters: ";
+    cin >> num2;
+
+    // Calculate the sum
+    float result = addCentimeters(num1, num2);
+
+    // Output: Display the result
+    cout << "The sum of " << num1 << " cm and " << num2 << " cm is " << result << " cm." << endl;
+
+    return 0;
+}
--- C++/Loops/loops.cpp
+++ C++/Loops/loops.cpp
@@ -9,7 +9,7 @@ int main(){
     cout<<"enter the number of tea cups to serve";
     cin>> teacups;
     while(teacups > 0){
-cout<<"servining cup of tea /n"<<teacups<<"reamaning"<<endl;
+cout<<"servining cup of tea \n"<<teacups<<"reamaning"<<endl;
 teacups--;
     }
     cout<<"all tea cups are served"<<endl;
